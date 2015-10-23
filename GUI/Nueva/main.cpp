f3d78152a940a4e8d9d3af995b16a665190bf541
Pancho <bertilxi@gmail.com>
2015-10-23 12:05:32 -0300

    dependencias inyectadas a interfaz principal

--- GUI/Nueva/main.cpp
+++ GUI/Nueva/main.cpp
@@ -1,10 +1,23 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include "mainwindow.h"
+
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    GestorBaseDatos gestorBaseDeDatos;
+    GestorCompetencias gestorCompetencias;
+    GestorLugares gestorLugares;
+    GestorParticipantes gestorParticipantes;
+    GestorPartidos gestorPartidos;
+    GestorUsuarios gestorUsuarios;
+
+    // comentado para pruebas
+    // MainWindow pegaso(0,gestorBaseDeDatos,gestorCompetencias,gestorLugares,gestorParticipantes,gestorPartidos,gestorUsuarios);
     MainWindow w;
+
+
     w.show();
 
     return a.exec();
