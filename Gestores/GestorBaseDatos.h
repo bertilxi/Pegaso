af96b021e7991d53e4adc5baf4562b1d2a44b2ac
Daniel Campodonico <d.campodonico@hotmail.com>
2015-10-23 13:38:43 -0300

    Added brief description for functions in GestorBD

--- Gestores/GestorBaseDatos.h
+++ Gestores/GestorBaseDatos.h
@@ -12,12 +12,14 @@ public:
     /**
      * @param obj
      * @param id
+     * @brief Carga un objeto haciendo una consulta con el id en la tabla correspondiente
      */
     template <class T>
     T virtual load(T obj, int id);
     
     /**
      * @param objs
+     * @brief Guarda una lista de objetos en su correspondiente tabla
      */
     template <class T1>
     void virtual save(QVector<T1> objs);
@@ -25,6 +27,7 @@ public:
     /**
      * @param objs
      * @param id
+     * @brief Guarda una lista de objetos que necesitan una fk pero el objeto no la conoce 
      */
     template <class T2>
     void virtual save(QVector<T2> objs, int id);
@@ -32,6 +35,7 @@ public:
     /**
      * @param obj1
      * @param obj2
+     * @brief Guarda una relacion n a n entre dos objetos
      */
     template <class T3,class T4>
     void virtual saveRelacion(T3 obj1, T4 obj2);
@@ -39,6 +43,7 @@ public:
     /**
      * @param obj
      * @param filtros
+     * @brief Ejecuta una consulta en la base de datos con filtros como parametros en select
      */
     template <class T5>
     QVector<T5> virtual query(T5 obj, QVector<QString> filtros);
