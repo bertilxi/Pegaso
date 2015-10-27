54d5e22cfb1bf21caaf82be64cdb31dfaffc3c7e
Daniel Campodonico <d.campodonico@hotmail.com>
2015-10-27 04:47:44 -0300

    Corregido algoritmo de fixture liga

--- Gestores/GestorPartidos.cpp
+++ Gestores/GestorPartidos.cpp
@@ -71,16 +71,25 @@ void GestorPartidos::generarFixtureLiga(Competencia *comp) {
 
     QVector<Participantes*> participantes=comp->getParticipantes();
     QVector<Partidos*> partidos;
-    //Algoritmo por matriz Round Robin
-    for (int i = 1; i < participantes.size(); ++i) {
-        for (int j = 0; j < participantes.size()-i; j++) {
-            Partido *part = new Partido;
-            part->setEquipoA(participantes[i-1]);
-            part->setEquipoB(participantes[i+j]);
-            part->setFecha(((i<<1)+j-1)%participantes.size());
-            partidos.push_back(part);
+    //Algoritmo Round Robin schedule de Édouard Lucas modificado
+    int n=participantes.size();
+        for (int i = 1; i < n; ++i) {
+            for (int j = 0; j < n - i - 1 + n&1 ; j++) { //Si son pares hago una iteración menos porque asigno especialmente al último
+                Partido *part = new Partido;
+                part->setEquipoA(participantes[i-1]);
+                part->setEquipoB(participantes[i+j]);
+                part->setFecha(((i<<1) + j - 1)%(n -1 + n&1 )+1); //La fecha la calculo por deducción de la matriz Round Robin
+                partidos.push_back(part);
+            }
+            //Si son pares asigno al último participante al partido que quedaría en la diagonal principal
+            if(!n&1){
+                Partido *part = new Partido;
+                part->setEquipoA(participantes[i-1]);
+                part->setEquipoB(participantes[n-1]);
+                part->setFecha(((i<<1)-2)%(n-1)+1);
+                partidos.push_back(part);
+            }
         }
-    }
     comp->setPartidos(partidos);
 }
 
