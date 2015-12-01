ef4ddf60b4aa0aaf4bb1d6dddf13e4cf0fb9f587
Daniel Campodonico <d.campodonico@hotmail.com>
2015-12-01 14:25:35 -0300

    Corregido generarFixture, ahora genera bien los partidos

--- GUI/test/GestorPartidos.cpp
+++ GUI/test/GestorPartidos.cpp
@@ -198,18 +198,19 @@ void GestorPartidos::generarFixtureLiga(Competencia *comp) {
     QVector<Partido*> partidos;
     //Algoritmo Round Robin schedule de Édouard Lucas modificado
     int n=participantes.size();
+    qDebug()<<"Numero de participantes"<<n;
         for (int i = 1; i < n; ++i) {
             for (int j = 0; j < n - i - 1 + (n&1) ; j++) { //Si son pares hago una iteración menos porque asigno especialmente al último
                 Partido *part = new Partido();
-                part->setEquipoA(participantes[i-1]);
+                part->setEquipoA(participantes[i-1]);qDebug()<<i-1<<"-- juega con "<<i+j;
                 part->setEquipoB(participantes[i+j]);
                 part->setFecha(((i<<1) + j - 1)%(n -1 + (n&1) )+1); //La fecha la calculo por deducción de la matriz Round Robin
                 partidos.push_back(part);
             }
             //Si son pares asigno al último participante al partido que quedaría en la diagonal principal
-            if(!n&1){
+            if(!(n&1)){
                 Partido *part = new Partido();
-                part->setEquipoA(participantes[i-1]);
+                part->setEquipoA(participantes[i-1]);qDebug()<<i-1<<"++ juega con "<<n-1;
                 part->setEquipoB(participantes[n-1]);
                 part->setFecha(((i<<1)-2)%(n-1)+1);
                 partidos.push_back(part);
